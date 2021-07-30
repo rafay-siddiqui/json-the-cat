4a7239043329f54fcac0dc0b45536be39cdaac6f
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-30 07:01:18 +0000

    Modularized code and added mocha chai test assertions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
 const request = require('request');
-const args = process.argv.splice(2);
 
-const breedSearch = args[0];
+const fetchBreedDescription = function(breedname, callback) {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, response, body) => {
-  if (error !== null) {
-    console.log("error: ", error);
-    return;
-  }
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedname}`, (error, response, body) => {
+    let desc;
+    if (!error && JSON.parse(body)[0]) {
+      desc = JSON.parse(body)[0].description;
+    } else if (!error && !JSON.parse(body)[0]) {
+      error = "Breed not found";
+    }
 
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Breed not found");
-    return;
-  }
+    callback(error, desc);
+  });
 
-  console.log(typeof data);
-  console.log(data[0].description);
-});
+};
+
+module.exports = {
+  fetchBreedDescription
+};
